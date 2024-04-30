531fd9e7bc28bc8e555cdd1c6f94a2c2b4ca0799
joshua-8 <joshua-8@users.noreply.github.com>
2024-04-30 05:43:15 +0000

    deploy: 046cac742c8d4baad0db3d08612773206f1ff25a

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['begin_0',['begin',['../class_byte_sized_encoder_decoder.html#a05da9f6f11790f4c3ee8c51228252dde',1,'ByteSizedEncoderDecoder']]],
+  ['begin_0',['begin',['../class_byte_sized_encoder_decoder.html#afaafc613073f19a83c58ca35a50412e1',1,'ByteSizedEncoderDecoder']]],
   ['bitcount_1',['bitCount',['../class_byte_sized_encoder_decoder.html#afef9f4cf7f22319d4dd75a6580370aaa',1,'ByteSizedEncoderDecoder']]],
   ['bytesizedencoderdecoder_2',['ByteSizedEncoderDecoder',['../class_byte_sized_encoder_decoder.html#a9bd8c4d4ec208c21b8b242905acef65f',1,'ByteSizedEncoderDecoder']]]
 ];
