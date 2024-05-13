6bc279939484261989c7e8d4832efece0f04a3d2
joshua-8 <joshua-8@users.noreply.github.com>
2024-05-13 17:04:46 +0000

    deploy: 2f7468403c3bd925679ba39fb191736a61d81592

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['address_0',['address',['../class_byte_sized_encoder_decoder.html#a67f158ba45e9058deb13c0dd74f65d77',1,'ByteSizedEncoderDecoder']]]
+  ['acknowledgements_0',['Acknowledgements',['../index.html#autotoc_md4',1,'']]],
+  ['address_1',['address',['../class_byte_sized_encoder_decoder.html#a67f158ba45e9058deb13c0dd74f65d77',1,'ByteSizedEncoderDecoder']]]
 ];
