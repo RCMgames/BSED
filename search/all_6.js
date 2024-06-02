16707c79c55341f6e9b0aa7ecc68a43f4b0ed1c9
joshua-8 <joshua-8@users.noreply.github.com>
2024-06-02 22:16:46 +0000

    deploy: e7d1fd530014e1fb585998c692609bd75ac072ad

--- search/all_6.js
+++ search/all_6.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['lastencodercount_0',['lastEncoderCount',['../class_byte_sized_encoder_decoder.html#ae3d2c555633d22b41be5bc5d787b35d4',1,'ByteSizedEncoderDecoder']]],
   ['lastreadmicros_1',['lastReadMicros',['../class_byte_sized_encoder_decoder.html#ae1d108b0c18ae5e00387b0a1b736af28',1,'ByteSizedEncoderDecoder']]],
-  ['links_2',['links',['../index.html#autotoc_md2',1,'']]]
+  ['lastvelocityencodercount_2',['lastVelocityEncoderCount',['../class_byte_sized_encoder_decoder.html#a3e0984e2337b36f16e0c6fe7d368532a',1,'ByteSizedEncoderDecoder']]],
+  ['links_3',['links',['../index.html#autotoc_md2',1,'']]]
 ];
