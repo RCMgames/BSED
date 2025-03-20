789dbea4e2f57efbc770d64bda909584ff535486
joshua-8 <joshua-8@users.noreply.github.com>
2025-03-20 05:32:32 +0000

    deploy: fb91fdadd0e6dffe0a2f97e4015170253bf47a3a

--- search/all_1.js
+++ search/all_1.js
@@ -4,5 +4,5 @@ var searchData=
   ['bill_20of_20materials_1',['Bill Of Materials',['../md_extras_2hardware_2_r_e_a_d_m_e.html#autotoc_md0',1,'']]],
   ['byte_20sized_20encoder_20decoder_2',['Byte Sized Encoder Decoder',['../index.html',1,'']]],
   ['byte_2dsized_2dencoder_2ddecoder_2eh_3',['byte-sized-encoder-decoder.h',['../byte-sized-encoder-decoder_8h.html',1,'']]],
-  ['bytesizedencoderdecoder_4',['bytesizedencoderdecoder',['../class_byte_sized_encoder_decoder.html',1,'ByteSizedEncoderDecoder'],['../class_byte_sized_encoder_decoder.html#aef89be72e2ae3c51790bceb77a95f96f',1,'ByteSizedEncoderDecoder::ByteSizedEncoderDecoder()']]]
+  ['bytesizedencoderdecoder_4',['ByteSizedEncoderDecoder',['../class_byte_sized_encoder_decoder.html',1,'ByteSizedEncoderDecoder'],['../class_byte_sized_encoder_decoder.html#aef89be72e2ae3c51790bceb77a95f96f',1,'ByteSizedEncoderDecoder::ByteSizedEncoderDecoder()']]]
 ];
--- search/pages_3.js
+++ search/pages_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['readme_0',['readme',['../md_extras_2firmware_2_r_e_a_d_m_e.html',1,'README'],['../md_extras_2hardware_2_r_e_a_d_m_e.html',1,'README']]]
+  ['readme_0',['README',['../md_extras_2firmware_2_r_e_a_d_m_e.html',1,'README'],['../md_extras_2hardware_2_r_e_a_d_m_e.html',1,'README']]]
 ];
